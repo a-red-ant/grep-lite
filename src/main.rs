6eb4976168da00e028f34cdab439c1382cb881ba
a-red-ant <cgr1963@gmail.com>
2022-03-19 19:18:39 +0000

    ch2-burfeader-lines.rs

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,15 @@ fn main() {
     // Creating a File requires a path argument and error handling in case the file
     // does not exist. This program crashes if a "readme.txt" is not present.
     let f = File::open("readme.md").unwrap();
-    let mut reader = BufReader::new(f);
+    let reader = BufReader::new(f);
 
-    // We’ll re-use a single String object over the lifet
-    let mut line = String::new();
-
-    // loop loops until the program encounters return, break or panics
-    loop {
-        // Reading from disk can fail and we need to explicitly handle this.
-        // In our case, errors crash the program.
-        let len = reader.read_line(&mut line).unwrap();
-        if len == 0 {
-            break;
-        }
-        println!("{} ({} bytes long)", line, len);
-
-        // Shrink the String back to length 0, preventing lines from persisting into the
-        // following ones
-        line.truncate(0);
+    // There is a subtle behavior change here.
+    // BufReader::lines() removes the trailing newline character
+    // from each line
+    for line_ in reader.lines() {
+        // Unwrapping the Result possible error at each line is
+        // still required, as with the manual version
+        let line = line_.unwrap();
+        println!("{} ({} bytes long)", line, line.len());
     }
 }
